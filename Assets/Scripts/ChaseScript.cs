c77a200846100628045719acd3947b40cbd0491c
SomeRandomPokemon42 <77223041+SomeRandomPokemon42@users.noreply.github.com>
2023-12-20 05:25:54 -0800

    UNLEASH THE TILES

--- Assets/Scripts/ChaseScript.cs
+++ Assets/Scripts/ChaseScript.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class ChaseScript : MonoBehaviour
 {
-    public GameObject ChaseObject;
-    public Vector3 Offset = Vector3.zero;
-    void Update()
-    {
-        Vector3 TrackTo = ChaseObject.transform.position - transform.position;
-        TrackTo += Offset;
-        transform.Translate(TrackTo * Time.deltaTime * 2);
-    }
+	public GameObject ChaseObject;
+	public float speed = 2;
+	public Vector3 Offset = Vector3.zero;
+	void Update()
+	{
+		Vector3 TrackTo = ChaseObject.transform.position - transform.position;
+		TrackTo += Offset;
+		Vector3 Movement = TrackTo * Time.deltaTime * speed;
+		Movement = new Vector3(
+			Mathf.Clamp(Movement.x, -Mathf.Abs(TrackTo.x), Mathf.Abs(TrackTo.x)),
+			Mathf.Clamp(Movement.y, -Mathf.Abs(TrackTo.y), Mathf.Abs(TrackTo.y)),
+			Mathf.Clamp(Movement.z, -Mathf.Abs(TrackTo.z), Mathf.Abs(TrackTo.z))
+		);
+		transform.Translate(Movement);
+	}
 }
