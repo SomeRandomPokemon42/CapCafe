7b9e50f6854014b650cfb4acab77c40bddfd7bf8
SomeRandomPokemon42 <77223041+SomeRandomPokemon42@users.noreply.github.com>
2024-01-04 11:22:25 -0800

    Fixed Stock Market

--- Assets/UI/Shop/StockMarketScript.cs
+++ Assets/UI/Shop/StockMarketScript.cs
@@ -1,20 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StockMarketScript : MonoBehaviour
 {
+	private Dictionary<string, int> StockPrices = new();
 	private TimeScript GameTime;
+	public float Varience = 2/3*100;
+
 	private void Start()
 	{
 		GameTime = GetComponent<TimeScript>();
+		GameTime.HourHasPassed.AddListener(ClearStocks);
 	}
-	public int STONKS(GameItem Item)
+	private void ClearStocks()
 	{
-		int Modifier = GameTime.day + SystemInfo.systemMemorySize + Item.Name.Length;
-		int NewValue = Mathf.RoundToInt(Mathf.PerlinNoise1D(Modifier) * Item.BaseValue + Item.BaseValue);
-		if (NewValue <= 0)
+		if (GameTime.hour == 24)
 		{
-			NewValue += (0 - NewValue) + 1;
+			StockPrices.Clear();
 		}
-		return NewValue;
+	}
+	public int STONKS(string ItemName, int baseValue)
+	{	
+		if (StockPrices.ContainsKey(ItemName))
+		{
+			return StockPrices[ItemName];
+		} else
+		{
+			// Create New Price
+			int lowEnd = Mathf.RoundToInt(baseValue / 100f * Varience);
+			int highEnd = Mathf.CeilToInt(baseValue / 100f * (100 + Varience));
+			if (lowEnd <= 0) { lowEnd = 1; }
+			int price = Random.Range(lowEnd, highEnd + 1);
+			StockPrices[ItemName] = price;
+			return price;
+		}
+	}
+	public int STONKS(GameItem item)
+	{
+		return STONKS(item.Name, item.BaseValue);
 	}
 }
