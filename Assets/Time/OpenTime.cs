b347927aa3315504befd07be647d2d5bb12fd4f5
SomeRandomPokemon42 <77223041+SomeRandomPokemon42@users.noreply.github.com>
2024-01-11 01:26:56 -0800

    Cafe openning

---
+++ Assets/Time/OpenTime.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenTime : MonoBehaviour
+{
+	[Header("GUI")]
+	[SerializeField] private Image[] GUIThings;
+	[SerializeField] private Sprite OpenTexture = null;
+	[SerializeField] private Sprite ClosedTexture = null;
+	Toaster toast = null;
+	TimeScript GameTime;
+	[Header("Time")]
+	public int OpenAt = 10;
+	public int CloseAt = 20;
+	[Header("Cafe")]
+	public bool Open = false;
+
+	private void Start()
+	{
+		toast = GetComponent<Toaster>();
+		MessWithGUI(false);
+		GameTime = GetComponent<TimeScript>();
+		GameTime.HourHasPassed.AddListener(ListenForTime);
+	}
+	private void ListenForTime()
+	{
+		if (GameTime.hour == OpenAt - 1)
+		{
+			toast.Toast("The Cafe opens in 1 hour!");
+		}
+		if (GameTime.hour == OpenAt)
+		{
+			OpenCafe();
+		}
+		if (GameTime.hour == CloseAt)
+		{
+			CloseCafe();
+		}
+	}
+	public void OpenCafe()
+	{
+		MessWithGUI(true);
+		toast.Toast("The Cafe is now open!");
+		Open = true;
+	}
+	public void CloseCafe()
+	{
+		MessWithGUI(false);
+		toast.Toast("The Cafe is now closed!");
+		Open = false;
+	}
+
+	public void MessWithGUI(bool Open)
+	{
+		foreach (Image thing in GUIThings)
+		{
+			if (Open)
+			{
+				thing.sprite = OpenTexture;
+			} else 
+			{ 
+				thing.sprite = ClosedTexture;
+			}
+		}
+	}
+}
