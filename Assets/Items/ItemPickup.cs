b02c49ba7376ad7ea0c6e5de49d5551cd83cdfef
SomeRandomPokemon42 <77223041+SomeRandomPokemon42@users.noreply.github.com>
2024-01-10 20:59:42 -0800

    Set up pathfinding

--- Assets/Items/ItemPickup.cs
+++ Assets/Items/ItemPickup.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-    public bool Reusable = false;
-    public int Uses = 1;
-    public GameItem item;
+	public bool Reusable = false;
+	public int Uses = 1;
+	public GameItem item;
 
-    public void Pickup()
-    {
-        Uses -= 1;
-        if (!GameObject.FindGameObjectWithTag("GameController").GetComponent<Directions>().PlayerInventory.AddItem(item))
-        {
-            //This returns false if the adding the item failed, so you get the use back
-            Uses += 1;
-        }
-        if (Reusable)
-        {
-            Uses += 1;
-        }
-        else
-        {
-            if (Uses < 1)
-            {
-                Destroy(gameObject);
-            }
-        }
-    }
+	public void Pickup()
+	{
+		Uses -= 1;
+		if (!GameObject.FindGameObjectWithTag("GameController").GetComponent<Directions>().PlayerInventory.AddItem(item))
+		{
+			//This returns false if the adding the item failed, so you get the use back
+			Uses += 1;
+		}
+		if (Reusable)
+		{
+			Uses += 1;
+		}
+		else
+		{
+			if (Uses < 1)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
 }
